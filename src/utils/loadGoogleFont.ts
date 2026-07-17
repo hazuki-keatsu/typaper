b04aeb14eb1f6ea107fed9723fee238e444b6480
叶月枫 <yeyuefeng699@qq.com>
2026-07-17 21:13:21 +0800

    feat: update font configuration to use Noto Serif SC

--- src/utils/loadGoogleFont.ts
+++ src/utils/loadGoogleFont.ts
@@ -36,14 +36,14 @@ async function loadGoogleFonts(
 > {
   const fontsConfig = [
     {
-      name: "IBM Plex Mono",
-      font: "IBM+Plex+Mono",
+      name: "Noto Serif SC",
+      font: "Noto+Serif+SC",
       weight: 400,
       style: "normal",
     },
     {
-      name: "IBM Plex Mono",
-      font: "IBM+Plex+Mono",
+      name: "Noto Serif SC",
+      font: "Noto+Serif+SC",
       weight: 700,
       style: "bold",
     },
